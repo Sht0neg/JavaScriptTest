1b7a944fcc0bf47399b0d532d42ff48b91e585c9
Admin <aper@gmail.com>
2025-03-03 21:00:29 +0300

    First

---
+++ hello.js
@@ -0,0 +1,27 @@
+let name = "Боба"
+const boolingMark = true
+
+console.log(1 / 0)
+console.log("M" / 0)
+
+let z = null
+let c
+
+console.log(z, c)
+
+let age = 15
+if (age > 10 && age < 18){
+    console.log("Плохо")
+}else if (age <= 10){
+    console.log("Совсем плохо")
+}
+
+for (let i = 0; i < 10; i++){
+    console.log(i)
+}
+
+function print(a){
+    console.log(a)
+}
+
+print(5)
