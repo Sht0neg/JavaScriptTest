17b80e141a2958e0af6938e2a72beb10bf1ccbf0
Admin <aper@gmail.com>
2025-03-17 21:17:12 +0300

    Sixth

--- fetch/script.js
+++ fetch/script.js
@@ -1,10 +1,23 @@
 const btn = document.querySelector(".btn-get")
+const btn2 = document.querySelector(".btn-del")
 const list = ["f.jpg", "s.jpg", "t.jpg", "fo.jpg"]
-let i = 0;
+let i = -1;
 btn.addEventListener("click", () => {
-    document.querySelector("img").setAttribute("src", list[i])
+    console.log(i)
     i++
     if (i == 4) {
         i = 0
     }
+    document.querySelector("img").setAttribute("src", list[i])
+})
+btn2.addEventListener("click", () => {
+    console.log(i)
+    i--
+    if (i == -2) {
+        i = 3
+    }
+    if (i == -1){
+        i = 3
+    }
+    document.querySelector("img").setAttribute("src", list[i])
 })
