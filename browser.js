4ada00d2aa6b95d94a8f50bd2911af4b7bf515b6
megatrc <Sh0tnik.19@yandex.ru>
2025-03-24 21:37:59 +0300

    Eleventh

--- browser.js
+++ browser.js
@@ -1,5 +1,5 @@
 //document.getElementsByClassName("btn-move")[0]
-const btn = document.querySelector("button.btn-move")
+const btn = document.querySelector("button")
 //document.querySelectorAll()
 btn.addEventListener("click", (event) =>{
     document.querySelector(".square").classList.toggle("square_active")
